359fbfe9ba46707e71d074d32ccb68f6d8e6723b
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2026-02-12 15:29:47 +0700

    finish vector dot product

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -33,12 +33,18 @@ public class VectorUtility {
         // TODO: Implement me properly!
         return new double[] { 0.0, 0.0, 0.0 };
     }
-    
+
     public double dotProduct(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return 0.0;
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Vectors must have the same dimension");
+        }
+        double result = 0.0;
+        for (int i = 0; i < v1.length; i++) {
+            result += v1[i] * v2[i];
+        }
+        return result;
     }
-    
+
     public double norm(double[] v1) {
         // TODO: Implement me properly!
         return 0.0;

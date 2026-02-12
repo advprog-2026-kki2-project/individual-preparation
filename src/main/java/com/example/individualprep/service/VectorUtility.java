39b4f88cf0bcd61f8e9af350d46e5a469d8c43f6
kerangleeya <shelia.vellicita13@gmail.com>
2026-02-12 15:15:25 +0700

    finish vector subtract feature

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -18,8 +18,15 @@ public class VectorUtility {
     }
 
     public double[] subtract(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Vectors must have the same dimension");
+        }
+
+        double[] result = new double[v1.length];
+        for (int i = 0; i < v1.length; i++) {
+            result[i] = v1[i] - v2[i];
+        }
+        return result;
     }
 
     public double[] multiply(double[] v1, int x) {
